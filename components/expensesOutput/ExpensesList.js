8a501dc8ee7d9f487581f0a183e973bae4907e1d
Zajjaj-Khan <kzajjaj@gmail.com>
2024-01-03 23:56:18 +0500

    Safety Commit before 12Am :)

--- components/expensesOutput/ExpensesList.js
+++ components/expensesOutput/ExpensesList.js
@@ -1,7 +1,8 @@
 import { StyleSheet, Text, View,FlatList } from 'react-native'
 import React from 'react'
+import ExpenseItem from './ExpenseItem'
 function renderExpenseItem(itemData){
-    return <Text>{itemData.item.description}</Text>
+    return <ExpenseItem {...itemData.item}/>
 }
 export default function ExpensesList({expenses}) {
   return <FlatList data={expenses} renderItem={renderExpenseItem} keyExtractor={(item)=>item.id}/>
